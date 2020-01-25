7863a6c1315b2e1cee8ae2485f6d192ad41f190b
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-01-25 14:58:47 -0700

    Add travel functions

--- bin/main.dart
+++ bin/main.dart
@@ -2,36 +2,63 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
+  printGreeting();
+  printIntroduction(responseToPrompt('What is your name?'));
+  print('Let\'s go on an adventure!');
+  travel(
+    promptForRandomOrSpecificDestination(
+      'Shall I randomly choose a planet for you to visit? (Y or N)'
+    )
+  );
+}
+
+void printGreeting() {
   print('Welcome to the Solar System!');
   print('There are 8 planets to explore.');
+}
 
-  print('What is your name?');
-
-  final name = stdin.readLineSync();
-
+void printIntroduction(String name) {
   print('Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
+}
 
+String responseToPrompt(String prompt) {
+  print(prompt);
+  return stdin.readLineSync();
+}
+
+void travelToRandomPlanet() {
   print(
-    'Let\'s go on an adventure!\n'
-    'Shall I randomly choose a planet for you to visit? (Y or N)'
+    'Ok! Traveling to Mercury...\n'
+    'Arrived at Mercury. A very hot planet, closest to the sun.'
   );
-  
+}
+
+void travelTo(String planetName) {
+  print(
+    'Traveling to $planetName...\n'
+    'Arrived at $planetName. A very cold planet, furthest from the sun.'
+  );
+}
+
+void travel(bool randomDestingation) {
+  if (randomDestingation) {
+    travelToRandomPlanet();
+  } else {
+    travelTo(responseToPrompt('Name the planet you would like to visit.'));
+  }
+}
+
+bool promptForRandomOrSpecificDestination(String prompt) {
   String answer;
-  
   while (answer != 'Y' && answer != 'N') {
-    answer = stdin.readLineSync();
+    answer = responseToPrompt(prompt);
     if (answer == 'Y') {
-    print(
-      'Ok! Traveling to Mercury...\n'
-      'Arrived at Mercury. A very hot planet, closest to the sun.'
-    );
+      return true;
+    } else if (answer == 'N') {
+      return false;
     } else {
-      print('Name the planet you would like to visit.');
-      final planetName = stdin.readLineSync();
-      print(
-        'Traveling to $planetName...\n'
-        'Arrived at $planetName. A very cold planet, furthest from the sun.'
-      );
+      print('Sorry, I didn\'t get that');
     }
   }
+  return false;
 }
