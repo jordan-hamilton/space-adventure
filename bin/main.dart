bc88556c0367009f1e93f237957bd849a6b8f486
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-01-24 20:26:25 -0700

    Add stagehand version

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:project_2/project_2.dart' as project_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_2.calculate()}!');
+}
---
+++ test/project_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_2/project_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
