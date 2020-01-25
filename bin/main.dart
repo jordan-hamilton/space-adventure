e3204918cec32fcedae602e105a8ac7a2259091d
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-01-24 20:44:07 -0700

    Prompt for user input

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,29 @@
-import 'package:project_2/project_2.dart' as project_2;
+// import 'package:project_2/project_2.dart' as project_2;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${project_2.calculate()}!');
+  print('Welcome to the Solar System!');
+  print('There are 8 planets to explore.');
+
+  print('What is your name?');
+
+  String name = stdin.readLineSync();
+
+  print('Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
+
+  print(
+    'Let\'s go on an adventure!\n'
+    'Shall I randomly choose a planet for you to visit? (Y or N)'
+  );
+
+  String answer = stdin.readLineSync();
+
+  print('Name the planet you would like to visit.');
+
+  String planetName = stdin.readLineSync();
+
+  print(
+    'Traveling to $planetName...\n'
+    'Arrived at $planetName. A very cold planet, furthest from the sun.'
+  );
 }
