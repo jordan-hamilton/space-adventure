fef6b12f1e72d0b321047f6176e99d3ca6c43d01
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-01-26 15:41:04 -0700

    Add methods for random planet and first planet with name

--- lib/src/planetary_system.dart
+++ lib/src/planetary_system.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'planet.dart';
 
 class PlanetarySystem {
+
+  final Random _random = Random();
   final String name;
   final List<Planet> planets;
 
@@ -8,4 +12,17 @@ class PlanetarySystem {
 
   int get numberOfPlanets => planets.length;
   bool get hasPlanets => planets.isNotEmpty;
+
+  Planet randomPlanet() {
+    if (!hasPlanets) return Planet.nullPlanet();
+    return planets[_random.nextInt(planets.length)];
+  }
+
+  Planet planetWithName(String name) {
+    return planets.firstWhere(
+      (planet) => planet.name == name,
+      orElse: () => Planet.nullPlanet()
+    );
+  }
+
 }
